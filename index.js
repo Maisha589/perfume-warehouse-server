4636c127bdabb2bd2a840f083b45f1cdf45d47a2
Maisha589 <maisharahman589@gmail.com>
2022-05-09 06:36:33 +0600

    user info

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("Perfume").collection("product");
+        const orderCollection = client.db("Perfume").collection("order");
 
         app.get("/products", async (req, res) => {
             const query = {};
@@ -24,7 +25,6 @@ async function run() {
 
         app.get("/products/:id", async (req, res) => {
             const id = req.params.id;
-            // const query = query = .toString()
             const query = { _id: ObjectId(id) }
             const perfume = await productCollection.findOne(query);
             res.send(perfume);
@@ -41,6 +41,27 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.put("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const newStock = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    stock: newStock.stock
+                }
+            };
+            const result = await productCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+        // Order
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
     }
